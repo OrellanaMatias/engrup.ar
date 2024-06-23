c74c6579e584a153cddf9715b1aec42ba61d1327
Orellana Matias <meorellanaramirez@gmail.com>
2024-06-23 20:14:50 -0300

    Correccion array icons

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ function processForm() {
   }
 
   const grupos = [];
-  const icons = ['img/icon1.png', 'img/icon2.png', 'img/icon3.png', 'img/icon4.png'];
+  const icons = ['img/icon1.png', 'img/icon2.png', 'img/icon3.png', 'img/icon4.png', 'img/icon5.png', 'img/icon6.png', 'img/icon7.png','img/icon8.png'];
 
   for (let i = 0; i < numero; i++) {
     grupos.push({
