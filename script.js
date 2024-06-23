49d4695d4a8cc9ec1be6f05022660fce41ebb4bd
Lexotanill <ianlucero2006@gmail.com>
2024-06-23 19:13:39 -0300

    las papu correciones ui y agregado de iconos fueron realizados

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const grupos = JSON.parse(localStorage.getItem('grupos')) || [];
   const groupList = document.getElementById('groupList');
 
-  const icons = ['img/icon1.png', 'img/icon2.png', 'img/icon3.png', 'img/icon4.png'];
+  const icons = ['img/icon1.png', 'img/icon2.png', 'img/icon3.png', 'img/icon4.png', 'img/icon5.png', 'img/icon6.png', 'img/icon7.png','img/icon8.png'];
 
   grupos.forEach((grupo, index) => {
     const groupItem = document.createElement('li');
@@ -87,7 +87,7 @@ function openModal(groupIndex, groupIconElement) {
 
   const grupos = JSON.parse(localStorage.getItem('grupos')) || [];
   const usedIcons = grupos.map(grupo => grupo.image);
-  const icons = ['img/icon1.png', 'img/icon2.png', 'img/icon3.png', 'img/icon4.png'];
+  const icons = ['img/icon1.png', 'img/icon2.png', 'img/icon3.png', 'img/icon4.png', 'img/icon5.png', 'img/icon6.png', 'img/icon7.png','img/icon8.png'];
 
   icons.forEach(icon => {
     const iconElement = document.createElement('img');
