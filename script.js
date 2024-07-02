df0280ac9a7dbca2f936390d6401836020ed5988
Orellana Matias <meorellanaramirez@gmail.com>
2024-07-02 09:41:14 -0300

    triste

--- script.js
+++ script.js
@@ -85,7 +85,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const toggle = document.createElement('div');
     toggle.className = 'toggle';
-    toggle.innerHTML = '<img src="img/flecha.webp" alt="Descripción de la imagen" style="width: 30px; height: 20px;">';
+    toggle.innerHTML = '<img src="img/flecha.webp" alt="si" style="width: 30px; height: 20px;">';
 
     const nameList = document.createElement('ul');
     nameList.className = 'name-list';
